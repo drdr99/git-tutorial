5301ed638a4bb8c8caeb5ea8429297a146b88755
Dragan Raseta <dragan.raseta.99@gmail.com>
2021-12-01 22:41:35 +0100

    Solved Coding Challange II

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main() {
-   printf("Hello ESPL!");
-   printf("New print!");
-   return 0;
+#include "espl_lib.h"
+
+int main() { 
+    unsigned int n;
+    char answer;
+    do{ 
+        printf("Enter a number: ");
+        scanf("%d", &n);  
+        printf("%s\n",num_to_words(n));
+        printf("Would you like to try another number [y/n]?");
+        scanf(" %c", &answer);
+    }while(answer == 'y');
+    return 0;
 }
